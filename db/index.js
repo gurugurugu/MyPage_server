d55dab9387209042f82e2e05d2ddcdead7f478a9
偉翔 增 <a556630702@gmail.com>
2023-03-07 01:41:58 +0800

    dbinfo

--- db/index.js
+++ db/index.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql')
 
 const db = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'ddd40702',
-    database: 'mypage',
+    host: 'sql12.freesqldatabase.com',
+    user: 'sql12603428',
+    password: 'xlEMtaTN4p',
+    database: 'sql12603428',
 })
 
 
